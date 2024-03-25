356fb42185a0a0cdb96f4c3161975ad4caa27643
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-03-24 23:22:12 -0300

    Cleaning up skeleton branch

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import Contact from "./components/Contact";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Hero from './components/Hero';
+import Skills from './components/Skills';
+
 
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
         <Route path ="/about" element = {<About/>} /> 
         <Route path="/projects" element={<ProjectsSection />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/skills" element={<Skills />} /> {/* Check this line */}
         {/* Add More Routes as needed */}
       </Routes>
       <Footer /> 
