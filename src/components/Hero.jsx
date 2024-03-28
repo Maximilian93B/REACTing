70ba4733e907766adcf727eeb5cc1d76b3d3029b
Maximilian93B <147316673+Maximilian93B@users.noreply.github.com>
2024-03-27 23:44:41 -0300

    Contact Done - Changed HomePage to proper BG/ Added Avatar SVG file

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { Parallax } from 'react-parallax';
-import BackgroundImage from '../assets/svg/ReactingHeroPage.svg';
+import BackgroundImage from '../assets/svg/ReactHeroBG.svg';
 import TheButton from './Button';
-
+import GameHeader from './RetroHeader';
 
 // Hero Section Container Styles 
 const HeroContainer = styled.section`
     height: 100vh;
+    width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -24,15 +25,33 @@ const HeroContent = styled.div`
     padding: 20px;
     margin-top: 100px;
 `; 
+// Flash for Hero Title effect
+const flash = keyframes`
+0%, 100% { 
+    opacity: 1;
+    color: #e60073; // Original neon-like color
+    text-shadow: 3px 3px 0 #000, 0 0 20px #e60073, 0 0 40px #e60073; // Original + enhanced glow
+  }
+  50% { 
+    opacity: 0;
+    color: #ff007a; // Lighter shade for a change in flash
+    text-shadow: none; // Temporarily remove shadow for effect
+  }
+`;
 
 const HeroTitle = styled.h1`
     font-size: 2.5rem;
     margin-bottom: 20px;
+    animation: ${flash} 1.5s linear infinite; // apply flash
 `;
 
-const HeroSubheading = styled.p`
-    font-size: 1.2rem;
-    margin-bottom: 40px;
+const ButtonContainer = styled.div`
+    text-align: center; // Center the content horizontally
+    position: absolute; // Positioning relative to the nearest positioned ancestor (HeroContainer)
+    top: 30%; // Adjust based on your layout
+    width: 100%; // Take the full width to center content correctly
+    z-index: 2; // Ensure it's above the background image
+
 `;
 
 const Hero = () => {
@@ -45,10 +64,13 @@ const Hero = () => {
             backgroundPosition: 'center center',
             backgroundRepeat: 'no-repeat',
           }}>
+            <ButtonContainer>
+          
+            </ButtonContainer>
         <HeroContainer>
             <HeroContent>
-                <HeroTitle>Welcome to the Future</HeroTitle>
-                <HeroSubheading>Explore my Projects and Journey though tech</HeroSubheading>
+                <HeroTitle>Loading...</HeroTitle>
+               
                 <TheButton /> {/* Button with Modal Cotent per location */}
             </HeroContent>
         </HeroContainer>
